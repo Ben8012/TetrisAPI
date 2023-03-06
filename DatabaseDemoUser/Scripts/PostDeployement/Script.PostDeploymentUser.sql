b7c9f3ba810fc6fa650e8385111224349069f5f2
Benjamin Sterckx <82587845+Ben8012@users.noreply.github.com>
2023-03-06 12:40:21 +0100

    V3

--- DatabaseDemoUser/Scripts/PostDeployement/Script.PostDeploymentUser.sql
+++ DatabaseDemoUser/Scripts/PostDeployement/Script.PostDeploymentUser.sql
@@ -1,8 +1,8 @@
 ﻿
 INSERT INTO [User] (Name,Email,Password,Point,IsActive) VALUES 
-('jean','jean@mail.com','Test1234=', 0,1),
-('delph','delph@mail.com','Test1234=',0,1),
-('ben','ben@mail.com','Test1234=',0,1)
+('Jean','jean@mail.com','Test1234=', 10,1),
+('Delph','delph@mail.com','Test1234=',15,1),
+('Ben','ben@mail.com','Test1234=',20,1)
 
 
 
